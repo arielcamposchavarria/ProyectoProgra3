c825f46068a7db5619e6dfbcc96d72c8a1d3f66d
arielcamposchavarria <128259451+arielcamposchavarria@users.noreply.github.com>
2023-09-29 10:44:28 -0600

    Add files via upload

---
+++ Proyecto/js/nose.js
@@ -0,0 +1,11 @@
+
+var imagen = document.createElement("img");
+
+imagen.src="logo.png";
+
+imagen.width = 750;
+imagen.height = 300;
+
+
+var contenedor = document.getElementById("contenedor-imagen");
+contenedor.appendChild(imagen);
